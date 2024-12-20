af9884d365ab7fba68ae7f9742c1a38e39277073
Alena Postnikova <ale671@mail.ru>
2024-12-20 17:27:47 +0300

    Classes for test renamed.

--- src/Lab2/CalculatorTest.java
+++ src/Lab2/CalculatorTest.java
@@ -2,25 +2,25 @@ package Lab2;
 
 import java.util.Scanner;
 
-public class Main {
+public class CalculatorTest {
     public static void main(String[] args){
 
         Scanner scanner = new Scanner(System.in);
 
-//        System.out.println("Введите два целых числа через Enter:");
-//
-//        int a = scanner.nextInt();
-//        int b = scanner.nextInt();
+        System.out.println("Введите два целых числа через Enter:");
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
 
 //        System.out.println("Введите два вещественных (через запятую) числа через Enter:");
 //
 //        double a = scanner.nextDouble();
 //        double b = scanner.nextDouble();
 
-        System.out.println("Введите два числа больше 2 млрд через Enter:");
-
-        long a = scanner.nextLong();
-        long b = scanner.nextLong();
+//        System.out.println("Введите два числа больше 2 млрд через Enter:");
+//
+//        long a = scanner.nextLong();
+//        long b = scanner.nextLong();
 
         System.out.println("Результат сложение чисел = "+Calculator.fn_sum(a,b));
         System.out.println("Результат умножения чисел = "+Calculator.fn_multiply(a,b));
--- src/Lab2/CatTest.java
+++ src/Lab2/CatTest.java
@@ -1,6 +1,6 @@
 package Lab2;
 
-public class Main2 {
+public class CatTest {
     public static void main(String[] args){
 
         Cat Guri = new Cat("oriental", "Gurgen","brown", "blue", 2 );
